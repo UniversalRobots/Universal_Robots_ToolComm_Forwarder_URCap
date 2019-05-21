6ca2242085eaf836378a68127394865b81ad8fc4
Lea Steffen <steffen@fzi.de>
2019-05-21 04:25:08 -0700

    debuag functionality

--- src/main/resources/com/fzi/rs485/impl/daemon/daemon-rs485.py
+++ src/main/resources/com/fzi/rs485/impl/daemon/daemon-rs485.py
@@ -8,6 +8,8 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 import subprocess
 import os
+import socket
+import logging
 
 
 title = ""
@@ -24,13 +26,53 @@ def create_file():
 
 def start_server(): 
 	print "start server"
-	cmd = 'echo "before" > log.txt' 
-	os.system(cmd) 
+	# cmd = 'echo "before" > log.txt' 
+	# os.system(cmd) 
 	cmd = './socat tcp-l:54321,reuseaddr,fork file:/dev/ttyTool,nonblock,raw,waitlock=/var/run/tty' 
 	os.system(cmd) 
-	cmd = 'echo "after" >> log.txt' 
-	os.system(cmd)
+	# cmd = 'echo "after" >> log.txt' 
+	# os.system(cmd)
 	print "end server"
-#create_file()
+
+
+def pop_up(): 
+	HOST = "192.168.0.104"
+	PORT = 30002
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
+	s.send("set_digital_out(2,True)" + "\n")
+	time.sleep(1)
+	
+	s.send("popup(\"Test rs daemon\", title=\"The Header\", blocking=True)" + "\n")
+	time.sleep(1)
+	s.send("set_digital_out(2,False)" + "\n")
+	time.sleep(1)
+
+	data = s.recv(1024)
+	print "Good bye!"
+
+def log_txt(): 
+	print "log"
+	
+	HOST = "192.168.0.104"
+	PORT = 30002
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
+	s.send("set_digital_out(2,True)" + "\n")
+	time.sleep(1)
+	
+	s.send("textmsg(\"Test rs daemon\")"+ "\n")
+	time.sleep(1)
+	s.send("set_digital_out(2,False)" + "\n")
+	time.sleep(1)
+
+	data = s.recv(1024)
+	print "Good bye!"
+
+# create_file()
+
 start_server()
 
+# pop_up()
+
+# log_txt()
