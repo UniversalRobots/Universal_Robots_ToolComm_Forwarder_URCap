ed9ade1b2a4fa625cc44279353216f9d83e21975
Lea Steffen <steffen@fzi.de>
2019-06-11 02:00:24 -0700

    -

--- src/main/resources/com/fzi/rs485/impl/daemon/daemon-rs485.py
+++ src/main/resources/com/fzi/rs485/impl/daemon/daemon-rs485.py
@@ -14,14 +14,37 @@ import logging
 
 title = ""
 
+# server:                ./socat tcp-l:54321,reuseaddr,fork file:/dev/ttyTool,nonblock,raw,waitlock=/var/run/tty
+
+def create_file():
+
+	info = "Hey start server"
+	file = open("in_file.txt", "w")
+	file.write(info)
+	file.close()
+	cmd = 'wc -l my_text_file.txt > out_file.txt'
+	os.system(cmd)
+
+
 def start_server(): 
-	print "start server"
-	# cmd = 'echo "before" > log.txt' 
-	# os.system(cmd) 
-	cmd = './socat tcp-l:54321,reuseaddr,fork file:/dev/ttyTool,nonblock,raw,waitlock=/var/run/tty' 
+
+	cmd = 'echo "before" > log.txt' 
 	os.system(cmd) 
-	# cmd = 'echo "after" >> log.txt' 
-	# os.system(cmd)
-	print "end server"
+	cmd = './socat tcp-l:54321,reuseaddr,fork file:/dev/ttyTool,nonblock,raw,waitlock=/var/run/tty' 
+	result = os.system(cmd) 
+	# result = "result"
+	cmd = 'echo "after" >> log.txt' 
+	os.system(cmd)
+	cmd = 'echo result >> log.txt' 
+	os.system(cmd)
+	#file = open("result.txt", "w")
+	#file.write(result)
+	#file.close()
+
+	cmd = 'wc -l my_text_file.txt > out_file.txt'
+	os.system(cmd)
 
 start_server()
+#create_file()
+
+
