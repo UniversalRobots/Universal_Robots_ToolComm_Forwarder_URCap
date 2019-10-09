dda6e0de99f429d47cbd5f1c27182e6ce1c20d5c
Felix Mauch <mauch@fzi.de>
2019-10-09 13:58:31 +0200

    Simplified python script

--- src/main/resources/com/fzi/rs485/impl/daemon/daemon-rs485.py
+++ src/main/resources/com/fzi/rs485/impl/daemon/daemon-rs485.py
@@ -1,41 +1,15 @@
 #!/usr/bin/env python
 
-import time
-import sys
-
-import xmlrpclib
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-
 import subprocess
-import os
-import socket
-import logging
-
-
-title = ""
-
-# server:                ./socat tcp-l:54321,reuseaddr,fork file:/dev/ttyTool,nonblock,raw,waitlock=/var/run/tty
-
-
 
 def start_server(): 
-
-	cmd = 'echo "before" > log.txt' 
-	os.system(cmd) 
+	
 	cmd = './socat tcp-l:54321,reuseaddr,fork file:/dev/ttyTool,nonblock,raw,waitlock=/var/run/tty' 
-	os.system(cmd) 
-	#result = subprocess.check_output(cmd) 
-	# result = "result"
-	cmd = 'echo "after" >> log.txt' 
-	os.system(cmd)
-
-	file = open("result.txt", "w")
-	file.write(result)
-	file.close()
-
-	#cmd = 'wc -l my_text_file.txt > out_file.txt'
-	#os.system(cmd)
+	try:
+		result = subprocess.check_output(cmd, shell=True)
+	except subprocess.CalledProcessError as err:
+		file = open("result.txt", "w")
+		file.write(str(err))
+		file.close()
 
 start_server()
-
-
